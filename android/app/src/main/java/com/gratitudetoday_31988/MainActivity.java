eed49a7020bfd055c162f9c914be83d378a51ac0
Crowdbotics <team@crowdbotics.com>
2021-11-14 16:29:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gratitudetoday_31988/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gratitudetoday_31988;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gratitudetoday_31988";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gratitudetoday_31988",
+  url: "https://gratitudetoday_31988.botics.co",
+  api: "https://gratitudetoday_31988.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
